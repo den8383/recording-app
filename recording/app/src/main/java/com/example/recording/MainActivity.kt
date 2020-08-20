c328d5ff68dc82df38389b6c2cd58aeef1779733
den8383 <den8383arfp@gmail.com>
2020-08-20 12:50:40 +0900

    [check] click button

--- recording/app/src/main/java/com/example/recording/MainActivity.kt
+++ recording/app/src/main/java/com/example/recording/MainActivity.kt
@@ -3,9 +3,18 @@ package com.example.recording
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button) as Button
+        button.setOnClickListener{
+            Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show()
+        }
+        
     }
 }
